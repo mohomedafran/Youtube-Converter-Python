27845591b1c2834518673a0c73d302a9abd018b1
Mohomed Afran <frmafran2004@gmail.com>
2025-01-19 18:33:43 +0530

    to vercel

--- convert.sh
+++
@@ -1,8 +0,0 @@
-clear
-echo -e "\e[32mDownloading Requiremwnts...\e[97m"
-echo ""
-pip install yt_dlp flask
-termux-setup-storage
-echo -e "\e[32mRequirements Downloaded Successfully\e[97m"
-echo""
-python main.py
